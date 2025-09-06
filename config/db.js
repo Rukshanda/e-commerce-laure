ac65a0200a127b37c77dfcaec2795996d6bf05bf
Rukshanda <rukshandanadir@gmail.com>
2025-09-06 15:00:30 +0500

    new

--- config/db.js
+++ config/db.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // wait 30 seconds for server
+    });
 
-    console.log("MangoDB working");
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.log("Some Error occured");
+    console.error("MongoDB connection error:", error); // show full error
   }
 };
 
